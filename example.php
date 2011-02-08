46508e2d9195b19dcc7267a7970cf7b62a24d42d
Christopher Chapman <christopher.chapman@me.com>
2011-02-08 14:48:00 -0500

    changed title

--- example.php
+++ example.php
@@ -2,7 +2,7 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8" />
-		<title>Interesting blog</title>
+		<title>File Smasher Tool</title>
 		<?php
 		/* 	Usage */
 			require_once('combine.php');
